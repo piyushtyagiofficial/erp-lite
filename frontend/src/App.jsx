95580166d616718dfd6159277cb4687332350d7b
piyush <tyagipiyush812@gmail.com>
2025-09-22 18:12:53 +0530

    Add Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Layout from './components/Layout';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <div className="App">
+        <Toaster position="top-right" />
+        <Layout>
+        </Layout>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
